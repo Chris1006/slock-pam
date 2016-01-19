70a5352770b00c7b2b93cc47272ff98302e3ecda
Miciah Masters <miciah.masters@gmail.com>
2016-01-18 19:47:12 -0500

    config.h: Initialize colorname using designators

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 static const char *const PAM_REALM = "login";
 
 static const char *colorname[NUMCOLS] = {
-	"black",     /* after initialization */
-	"#005577",   /* during input */
-	"#CC3333",   /* failed/cleared the input */
+	[INIT]  = "black",
+	[INPUT] = "#005577",
+	[EMPTY] = "#CC3333",
 };
